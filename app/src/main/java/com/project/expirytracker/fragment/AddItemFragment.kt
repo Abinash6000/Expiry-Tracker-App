47fee8a13455425dd57e78bce10c9092ba9fa5c2
Abinash6000 <abinashdash6000@gmail.com>
2024-02-15 22:22:36 +0530

    bug fix

--- app/src/main/java/com/project/expirytracker/fragment/AddItemFragment.kt
+++ app/src/main/java/com/project/expirytracker/fragment/AddItemFragment.kt
@@ -3,6 +3,7 @@ package com.project.expirytracker.fragment
 import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -49,7 +50,7 @@ class AddItemFragment : Fragment() {
             val itemQuantity:Short = Integer.parseInt(updateTIL(binding.enterQuantity,binding.itemNameTIL2)).toShort()
             val price:Short = Integer.parseInt(updateTIL(binding.enterPrice,binding.itemNameTIL5)).toShort()
 
-            if(itemName.toInt() != 0 && itemType.toInt() != 0 && itemQuantity.toInt() != 0 && price.toInt() != 0
+            if(itemName != "0" && itemType != "0" && itemQuantity != 0.toShort() && price != 0.toShort()
                 && !binding.enterMfg.text.isNullOrBlank() && !binding.expDate.text.isNullOrBlank()) {
                 val databaseModel =
                     DatabaseModel(
